e95fe26ae4340dc8143348608b666babd2178e77
Piero Sabino <piero.sa@icloud.com>
2021-09-17 20:04:28 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Book from './components/Book';
+import HomeSearch from './components/HomeSearch';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+          <Switch>
+            <Route path="/" exact component={HomeSearch} />
+            <Route path="/book/:id" componment={Book} />
+          </Switch>
+      </Router>
     </div>
   );
 }
