01eaa97d3ac8842e48ea9784bef26202fb2ada70
Piero Sabino <piero.sa@icloud.com>
2021-09-18 22:47:13 +0200

    Update Project

--- src/components/SearchBook.js
+++ src/components/SearchBook.js
@@ -1,11 +1,29 @@
 import React from "react";
 import { Button, InputGroup, FormControl} from 'react-bootstrap';
-
+import axios from "axios";
 
 function SearchBook () {
+
+const handleSubmit = (e) =>{
+    e.preventDefault();
+
+    const query = 'React';
+
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${query}`;
+
+    axios.get(url)
+    .then(res=>{
+        console.log(res);
+    })
+    .catch(err => {
+        console.log(err.response);
+    });
+}
+
+
         return (
             <div>
-                <InputGroup className="mb-3 form">
+                <InputGroup className="mb-3 form" onSubmi={handleSubmit}>
                     <FormControl
                     placeholder="Search book..."
                     aria-label="search-book"
