1805850edbce92a10cac2d753dd2357b8ec642fc
Piero Sabino <piero.sa@icloud.com>
2021-09-22 14:37:15 +0200

    Update project

--- src/pages/HomeSearch.js
+++ src/pages/HomeSearch.js
@@ -56,7 +56,6 @@ return(
                             placeholder='Search book...'
                             aria-label='search-book'
                             aria-describedby='basic-addon2'
-                            className="searchbar"
                             name='query'
                             value={query}
                             onChange={handleChange}
